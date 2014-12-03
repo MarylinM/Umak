66a90bc5eae7a297d27a9fdfa1241f0b05cda243
Marylin <Marylin@Magda>
2014-12-03 13:11:19 -0600

    modificacion de view_bienvenida.php

---
+++ application/views/view_bienvenida.php
@@ -0,0 +1,12 @@
+<html>
+
+    <head>
+    <title>Nombre o clave de usuario incorrectos</title>
+    <h1>No tiene permiso para acceder</h1>
+        
+    </head>
+    <body>
+
+    </body>
+    
+</html>
